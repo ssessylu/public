cbc2fe540aa3ceeb25058fac3ec2f0cefd69e492
ssessylu <94691217+ssessylu@users.noreply.github.com>
2022-06-28 01:44:03 +0100

    Add files via upload

---
+++ topbarmodule.js
@@ -0,0 +1,34 @@
+$("#topbarmodule").html(`
+<div class="navbar navbar-static">
+    <nav class="navbar-main navbar-color nav-collapsible sideNav-lock topbar-background">
+        <div class="nav-wrapper">
+            <!--
+            <div class="" style="margin-left: 10px; display: inline-flex; align-items: center; ">
+            </div>
+            <ul class="navbar-list right">
+                <li><a class="waves-effect waves-block waves-light toggle-fullscreen" href="javascript:void(0);"><i
+                            class="material-icons">settings_overscan</i></a>
+                </li>
+                <li><a class="waves-effect waves-block waves-light profile-button" href="javascript:void(0);"
+                        data-target="profile-dropdown">
+                        <i class="material-icons">settings</i>
+                    </a>
+                </li>
+            </ul>
+            -->
+            <!-- profile-dropdown-->
+            <!--
+            <ul class="dropdown-content" id="profile-dropdown">
+                <li><a class="grey-text text-darken-1"><i class="material-icons">settings_input_svideo</i>
+                        Settings</a></li>
+
+                <li class="divider"></li>
+                <li><a class="grey-text text-darken-1"><i class="material-icons">lock_outline</i>
+                        Logout</a></li>
+            </ul>
+            -->
+        </div>
+    </nav>
+</div>
+
+`);
